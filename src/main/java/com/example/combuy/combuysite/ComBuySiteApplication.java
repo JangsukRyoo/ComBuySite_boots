7ccf4fc06d4f5f1402fc415c16a048d60446b1da
JangsukRyoo <jangsukr@gmail.com>
2022-02-01 00:29:40 +0900

    Initial commit

---
+++ src/main/java/com/example/combuy/combuysite/ComBuySiteApplication.java
@@ -0,0 +1,13 @@
+package com.example.combuy.combuysite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComBuySiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComBuySiteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/combuy/combuysite/ComBuySiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.combuy.combuysite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComBuySiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
